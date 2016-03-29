81fa8481de5399b7f9e29e078aaf8515e516a070
Marquand <marquand.thibault@gmail.com>
2016-03-29 20:32:20 +0200

    'final GitLab-Save' - 'Tue Mar 29 20:31:53 CEST 2016'

--- GitLab-Start.sh
+++ GitLab-Start.sh
@@ -13,7 +13,7 @@ now="$(date)"
 #printf "%s\n" "$now"
 NOW=$(echo $now|sed s/' '/'\-'/g)
 
-VERSION=1.3
+VERSION=1.4
 
 echo ""
 echo "----------------------------------------------------------------------------"
@@ -42,13 +42,15 @@ then
 	mv GitLab-Start.sh maj/GitLab-Start-OLD.sh
 	rm GitLab-Start.sh
 	curl -o GitLab-Start.sh "http://vps241658.ovh.net/script/GitLab-Start.sh"
-	chmod 777 GitLab-Start.sh	
+	chmod 777 GitLab-Start.sh
+	echo "----------------------------------------------------------------------------"
 	echo -e "------ Launcher : [\033[36mOK\033[0m]"
 	echo ""
 	echo ""
 
 	curl -o maj/maj_windows.sh "http://vps241658.ovh.net/script/maj_windows.sh"
 	curl -o maj/maj_mac.sh "http://vps241658.ovh.net/script/maj_mac.sh"
+	echo "----------------------------------------------------------------------------"
 	echo -e "------ Config : [\033[36mOK\033[0m]"
 	echo ""
 	echo ""
@@ -57,6 +59,7 @@ then
 	rm GitLab-Save.sh
 	curl -o GitLab-Save.sh "http://vps241658.ovh.net/script/GitLab-Save.sh"
 	chmod 777 GitLab-Save.sh
+	echo "----------------------------------------------------------------------------"
 	echo -e "------ Core : [\033[36mOK\033[0m]"
 	echo ""
 	echo ""
