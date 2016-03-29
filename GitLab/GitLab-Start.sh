17f8a35feddaf6bb9b78772a93b1cee73f5278eb
Marquand <marquand.thibault@gmail.com>
2016-03-29 20:24:42 +0200

    'Final Script GitLab sgestion màj' - 'Mar 29 mar 2016 20:23:35 CEST'

--- GitLab/GitLab-Start.sh
+++ GitLab/GitLab-Start.sh
@@ -13,18 +13,16 @@ now="$(date)"
 #printf "%s\n" "$now"
 NOW=$(echo $now|sed s/' '/'\-'/g)
 
+VERSION=1.2
+
 echo ""
 echo "----------------------------------------------------------------------------"
 echo -e "------ Service Mise à Jour GitLab-Save de [\033[32mMediashare.fr\033[0m]"
+echo -e "------ [\033[36mVersion : $VERSION\033[0m]"
 echo "----------------------------------------------------------------------------"
-
-
-path=`echo "$0" | sed -e "s/[^\/]*$//"`
-mkdir maj
-curl -o $path/maj/maj_windows.sh "http://vps241658.ovh.net/script/maj_windows.sh"
-curl -o $path/maj/maj_mac.sh "http://vps241658.ovh.net/script/maj_mac.sh"
-
-rm GitLab-Save.sh
+echo ""
+echo ""
+echo ""
 
 echo -e -n "2 versions sont disponnibles : [\033[36mMac -> (mac)\033[0m] & [\033[32mWindows -> (win)\033[0m] :"
 read TYPE
@@ -32,20 +30,45 @@ TYPE=${TYPE:-win}
 
 if [ "$TYPE" = "win" ]
 then
-	curl -o $path/maj_windows.sh "http://vps241658.ovh.net/script/maj_windows.sh"
-	./maj_windows.sh
+	mv GitLab-Start.sh GitLab-Start-OLD.sh
+	path=`echo "$0" | sed -e "s/[^\/]*$//"`
+	curl -o $path/maj_windows.sh "http://vps241658.ovh.net/script/GitLab-Save.sh"
 fi
 if [ "$TYPE" = "mac" ]
 then
-	wget http://vps241658.ovh.net/script/GitLab-Save.sh
+
+	mkdir maj
+	
+	mv GitLab-Start.sh maj/GitLab-Start-OLD.sh
+	rm GitLab-Start.sh
+	curl -o GitLab-Start.sh "http://vps241658.ovh.net/script/GitLab-Start.sh"
+	chmod 777 GitLab-Start.sh	
+	echo -e "------ Launcher : [\033[36mOK\033[0m]"
+	echo ""
+	echo ""
+
+	curl -o maj/maj_windows.sh "http://vps241658.ovh.net/script/maj_windows.sh"
+	curl -o maj/maj_mac.sh "http://vps241658.ovh.net/script/maj_mac.sh"
+	echo -e "------ Config : [\033[36mOK\033[0m]"
+	echo ""
+	echo ""
+
+	mv GitLab-Save.sh maj/GitLab-Save-OLD.sh
+	rm GitLab-Save.sh
+	curl -o GitLab-Save.sh "http://vps241658.ovh.net/script/GitLab-Save.sh"
+	chmod 777 GitLab-Save.sh
+	echo -e "------ Core : [\033[36mOK\033[0m]"
+	echo ""
+	echo ""
 fi
 
-echo -e -n "[\033[32mLancer GitLab-Save\033[0m] [\033[31my/n\033[0m] :"
+echo -e -n "[\033[32mLancer GitLab-Save\033[0m] [\033[31mYes(y)/No(n)/Restart(r)\033[0m] :"
 read START
 if [ "$START" = "y" ]
 then
 	bash GitLab-Save.sh
 fi
-
-rm maj_windows.sh
-rm maj_mac.sh
+if [ "$START" = "r" ]
+then
+	bash GitLab-Start.sh 
+fi
--- maj/maj_windows.sh
+++ maj/maj_windows.sh
@@ -1,4 +1,2 @@
 path=`echo "$0" | sed -e "s/[^\/]*$//"`
-
-mv GitLab-Start.sh GitLab-Start-OLD.sh
 curl -o $path/GitLab-Start.sh "http://vps241658.ovh.net/script/GitLab-Start.sh"
