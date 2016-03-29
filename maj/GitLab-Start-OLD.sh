2550a06d53bdb66499f46989ae133cf277a4964e
Marquand <marquand.thibault@gmail.com>
2016-03-29 22:17:57 +0200

    '' - 'Tue Mar 29 22:17:48 CEST 2016'

--- maj/GitLab-Start-OLD.sh
+++ maj/GitLab-Start-OLD.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-# Author:Mediashare
+# Author:Mediashare - Marquand Thibault
 # Site:Mediashare.fr
 
-# remgit.sh
-# Creates a remote git repository from the current local directory
+# Prérequis :
+#   - Curl
+#   - Git
+#   - Compte GitLab
 
-# Configuration
-# Replace SSH_USERNAME, SSH_HOST, SSH_GIT_PATH with your details
+# Utilisation :
+#   - Lancer ./GitLab-Start.sh
+#   - Si besoin, faite l'installation ou la mise à jour sur le système d'exploitation que vous souhaitez.
+#   - Vous pouvez attendre qu'on vous propose de lancer le script de sauvegarde ou directement avec ./GitLab-Save.sh
+#   - Une fois dans le service de sauvergarde, vous avec plusieurs options :
+#       - Init (i) : Initialisation du projet, vous devez créer un répertoire sur GitLab afin de stocker la sauvegarde.
+#       - Push (p) : Mettre à jour votre sauvegarde sur GitLab.
+#       - Clone (c) : Après une Initialisation, récupère une copie des fichiers sauvegardés.
+#       - Steal (s) : Suspect.
+
+# Patch Note :
+#   - 1.7.5 :
+#       - Ajout du système de Mise à jour.
+#       - Sortie Public.
+#
+
 
 now="$(date +%Y-%m-%d)"
 #printf "%s\n" "$now"
 NOW=$now
 #VERSION="1.7.3 - $(echo $now)"
-VERSION="1.7.4"
+VERSION="1.7.5"
 
 echo ""
 echo "----------------------------------------------------------------------------"
